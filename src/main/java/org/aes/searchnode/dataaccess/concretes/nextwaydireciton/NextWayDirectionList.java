0cb9c0983cda14bafc62a16e7a9b0d279840e89f
Ahmet_Emin <ahmeteminsaglik@gmail.com>
2022-11-24 19:44:28 +0300

    getAll() getAllReverse() functions are created, close #

--- src/main/java/org/aes/searchnode/dataaccess/concretes/nextwaydireciton/NextWayDirectionList.java
+++ src/main/java/org/aes/searchnode/dataaccess/concretes/nextwaydireciton/NextWayDirectionList.java
@@ -5,15 +5,18 @@ import org.aes.searchnode.core.utilities.DataResult;
 import org.aes.searchnode.core.utilities.ErrorDataResult;
 import org.aes.searchnode.core.utilities.SuccessDataResult;
 import org.aes.searchnode.dataaccess.abstracts.ReachableNextWayDirection;
+import org.aes.searchnode.dataaccess.comparator.ComparatorNextWayDirectionRequiredData;
 import org.aes.searchnode.entities.concretes.NextWayDirectionRequiredData;
 import org.aes.searchnode.entities.concretes.PriorityChar;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
 public class NextWayDirectionList<T> implements ReachableNextWayDirection<T> {
-    private List<NextWayDirectionRequiredData> list = new ArrayList<>();
+    private List<NextWayDirectionRequiredData<T>> list = new ArrayList<>();
+    private static final ComparatorNextWayDirectionRequiredData comparatorNextWayDirectionRequiredData = new ComparatorNextWayDirectionRequiredData();
 
     @Override
     public DataResult<SearchNode<T>> getNextWayOfChar(PriorityChar pc) {
@@ -32,11 +35,24 @@ public class NextWayDirectionList<T> implements ReachableNextWayDirection<T> {
     public DataResult<SearchNode<T>> addPossibilityNWDNodeToReachableNWD(PriorityChar pc, SearchNode<T> searchNode) {
 //        System.out.println("!!!!! listee Veri ekleniyor : "+pc+"  /  "+searchNode);
         list.add(new NextWayDirectionRequiredData(pc, searchNode));
+
+        if(list.size()>1){
+            System.out.println("-----------------------------------");
+            System.out.println("siralama oncesi : ");
+            list.forEach(e-> System.out.println(e));
+        }
+
+
+
+        if(list.size()>1){System.out.println("siralama sonrasi: ");
+        list.forEach(e-> System.out.println(e));
+        System.out.println("==========================================");}
         return new SuccessDataResult<>("--> SearchNode is added to List");
     }
 
     @Override
     public List<NextWayDirectionRequiredData> getAllDataOfSearchNode() {
+        Collections.sort(list, new ComparatorNextWayDirectionRequiredData());
         return new ArrayList<>(list);
 //        return null;
     }
---
+++ src/main/java/org/aes/searchnode/dataaccess/concretes/orderby/Order.java
@@ -0,0 +1,15 @@
+/*
+package org.aes.searchnode.dataaccess.concretes.orderby;
+
+import java.util.Comparator;
+
+public class Order {
+
+    public static Comparator getComparator(OrderBy orderBy) {
+        if (orderBy == OrderBy.DESC) {
+            return new ComparatorDESC().getDoubleComparator();
+        }
+        return new ComparatorASC().getDoubleComparator();
+    }
+}
+*/
